08f01f4b5240367bc1e6f3604cdc5f9c5f22447a
WanderWashington <wander.wsantos@gmail.com>
2021-10-25 03:09:12 -0300

    Seção 11 : Implementação de Migrations e reestruturação do projeto

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Cayo', 'Male', 'Perico'),
+	(2, 'USA', 'Miguel', 'Male', 'Madrazo'),
+	(3, 'USA', 'Trevor', 'Male', 'De Santa');
